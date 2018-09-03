766cd83af8179f6bf6b221715ea2977f350d3eee
CharlesGarrocho <charles.garrocho@gmail.com>
2018-09-03 17:07:11 -0300

    corrigido: template.

--- moises/camera/readcamera.py
+++ moises/camera/readcamera.py
@@ -12,6 +12,9 @@ def CaptureFrame():
     # Capture frame
     for i in range(0,5):
         ret, frame = cap.read()
+    
+    frame = cv2.GaussianBlur(frame,(95,95),3)
+    frame = frame[:,200:550]
 
     cv2.imwrite('Foto.png', frame)
 
@@ -25,21 +28,22 @@ def FindTemplate():
     w, h = template.shape[::-1]
     
     res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-    threshold = 0.8
+    threshold = 0.9
     loc = np.where( res >= threshold)
-    try:
-        pt = zip(*loc[::-1])[0]
-        py = (392-pt[0])/14 - 4
-        px = (343-pt[1])/14 + 9
-        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
-    except:
-        py = 0
-        px = 5
-    print("Point: "+ str(px) + "," + str(py))
-    
+    #try:
+    pt = zip(*loc[::-1])[0]
+    # pt[0] eixo y, pt[1] eixo x 
+    print 'X: ', pt[1], ' Y: ', pt[0]
+    py = (392-pt[0])/14 - 4
+    px = (343-pt[1])/14 + 9
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    #except:
+    #py = 0
+    #px = 5
+    #print("Point: "+ str(px) + "," + str(py))
     cv2.imwrite('res.png',img_rgb)
     return [px,py]
 
-#CaptureFrame()
-#p = FindTemplate()
-#print (str(p[0]) + " " + str(p[1]) + " 0")
+CaptureFrame()
+p = FindTemplate()
+print (str(p[0]) + " " + str(p[1]) + " 0")
