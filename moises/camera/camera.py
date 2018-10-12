cdce186e7327d9a7015ea124962ee3823f8e858c
Thiago D'Angelo <eng.thiago.d.angelo@gmail.com>
2018-10-12 10:19:43 -0300

    Update camera configuration

--- moises/camera/camera.py
+++ moises/camera/camera.py
@@ -1,107 +1,112 @@
 import cv2
 import numpy as np
 
+
 def open_cap():
-    cap = cv2.VideoCapture(0)
-    
-    #Setting Camera Resolution
+    cap = cv2.VideoCapture('http://haddad.local:6060/?action=stream')
+
+    # Setting Camera Resolution
     cap.set(3, 640)
     cap.set(4, 480)
-    
+
     return cap
-    
+
+
 def cap_frame(cap):
-    
     # Capture frame
-    for i in range(0,20):
+    for i in range(0, 20):
         ret, frame = cap.read()
-    
-    img = cv2.GaussianBlur(frame,(75,75),3)
-    #img = frame[:,200:550]
-    img = frame[45:460,200:500]
-    img = cv2.bilateralFilter(img,15,75,75)
-    
+
+    img = cv2.GaussianBlur(frame, (75, 75), 3)
+    # img = frame[:, 230:610]
+    img = frame[:, 230:550]
+    img = cv2.bilateralFilter(img, 15, 75, 75)
+
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
-    #cv2.imwrite('Foto1.png', gray)
-    
+
+    # cv2.imwrite('Foto1.png', gray)
+
     return gray
-    
+
+
 def find_red_temp(gray):
 
     template = cv2.imread('tv.png')
     template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-    
+
     # run template matching, get minimum val
     res = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-    
+
     min_thresh = 0.75
-    match_locations = np.where(res>min_thresh)
-    
+    match_locations = np.where(res > min_thresh)
+
     # draw template match boxes
     w, h = template.shape[::-1]
     rets = []
     for (x, y) in zip(match_locations[1], match_locations[0]):
         ok = False
-        if len(rets) == 0  and y < 235:
-            rets.append((x,y))
+        if len(rets) == 0 and y < 280:
+            rets.append((x, y))
         else:
             ok = False
             for i in rets:
-                rx = abs(i[0]-x)
-                ry = abs(i[1]-y)
-                if rx > 15 and ry > 15 and y < 235:
+                rx = abs(i[0] - x)
+                ry = abs(i[1] - y)
+                if rx > 15 and ry > 15 and y < 280:
                     ok = True
                 else:
                     ok = False
             if ok:
-                rets.append((x,y))
+                rets.append((x, y))
     if len(rets) > 0:
         return str(rets[0][0]) + ' ' + str(rets[0][1]) + ' 0'
     else:
-        return '0 0 0'
-                
+        return '-1 -1 -1'
+
+
 def find_black_temp(gray):
-    
+
     template = cv2.imread('tp.png')
     template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-    
+
     # run template matching, get minimum val
     res = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-    
+
     min_thresh = 0.75
-    match_locations = np.where(res>min_thresh)
-    
+    match_locations = np.where(res > min_thresh)
+
     # draw template match boxes
     w, h = template.shape[::-1]
     rets = []
     for (x, y) in zip(match_locations[1], match_locations[0]):
         ok = False
-        if len(rets) == 0  and y > 175:
-            rets.append((x,y))
+        if len(rets) == 0 and y > 220:
+            rets.append((x, y))
         else:
             ok = False
             for i in rets:
-                rx = abs(i[0]-x)
-                ry = abs(i[1]-y)
-                if rx > 15 and ry > 15 and y > 175:
+                rx = abs(i[0] - x)
+                ry = abs(i[1] - y)
+                if rx > 15 and ry > 15 and y > 220:
                     ok = True
                 else:
                     ok = False
             if ok:
-                rets.append((x,y))
+                rets.append((x, y))
     if len(rets) > 0:
         return str(rets[0][0]) + ' ' + str(rets[0][1]) + ' 0'
     else:
-        return '0 0 0'
+        return '-1 -1 -1'
+
 
 def get_caps(cap):
     gray = cap_frame(cap)
     red = find_red_temp(gray)
     black = find_black_temp(gray)
     return red, black
+
 
 cap = open_cap()
 
--- moises/camera/servidor.py
+++ moises/camera/servidor.py
@@ -1,24 +1,7 @@
-#import rbc
 import threading
 import socket
 
-'''global black, red
-black = []
-red = []
-
-MUTEX = threading.Lock()
-
-def camera():
-    global red, black
-    while True:
-        print 'Get Caps...', red, black
-        r, b = rbc.get_caps()
-        #MUTEX.acquire()
-        red, black = r, b
-        #MUTEX.release()
-'''
 def new_client(conn, addr):
-    #global red, black
     print 'Connection address:', addr
     
     while True:
@@ -29,34 +12,19 @@ def new_client(conn, addr):
 
         if '0' in data:
             conn.send(pontos[0])
-            #if len(red) > 0:
-                #MUTEX.acquire()
-                #conn.send(str(red[0][0]) + " " + str(red[0][1]) + " 0")
-                #MUTEX.release()
-            #else:
-                #conn.send("0 0 0")
         else:
             conn.send(pontos[1])
-            #if len(black) > 0:
-                #MUTEX.acquire()
-                #conn.send(str(black[0][0]) + " " + str(black[0][1]) + " 0")
-                #MUTEX.release()
-            #else:
-                #conn.send("0 0 0")
 
     conn.close()
 
 MUTEX = threading.Lock()
-TCP_IP = '192.168.0.153'
+TCP_IP = '0.0.0.0'
 TCP_PORT = 5005
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+BUFFER_SIZE = 20
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
-
-#c = threading.Thread(target=camera, args=())
-#c.start()
 
 while True:
     t_c = threading.Thread(target=new_client, args=(s.accept()))
